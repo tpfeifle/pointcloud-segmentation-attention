9b5ecadc33057f488f88bb53a8bad23f5a618b19
Tim Pfeifle <pfeifletim@gmail.com>
2019-08-02 09:49:12 +0200

    Predictions without features + qualitative visualization results

---
+++ attention_points/download_for_visualization.sh
@@ -0,0 +1,3 @@
+scp tim@maxrieger.ddns.net:/home/tim/results/for_visualization/points/$1.npy /Users/tim/Downloads/for_visualization/points/$1.npy
+scp tim@maxrieger.ddns.net:/home/tim/results/for_visualization/labels/$1.npy /Users/tim/Downloads/for_visualization/labels/$1.npy
+scp tim@maxrieger.ddns.net:/home/tim/results/for_visualization/groundtruth_labels/$1.npy /Users/tim/Downloads/for_visualization/groundtruth_labels/$1.npy
